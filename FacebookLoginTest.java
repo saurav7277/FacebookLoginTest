f6894fbeb3eb3d39bb5c58eb1766da99ccb3105c
saurav <sauravkumar1995@gmail.com>
2019-08-26 14:52:27 +0530

    jsbab

---
+++ FacebookLoginTest.java
@@ -0,0 +1,164 @@
+package com.facebooklogin;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.NumberToTextConverter;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+
+
+public class FacebookLoginTest
+{
+	WebDriver driver;
+	WebDriverWait wait;
+	
+	//method for executing setup process before executing test
+	@BeforeTest
+	public void setup()
+	{
+		System.setProperty("webdriver.chrome.driver","D:\\QA-Testing\\chromedriver_win32\\chromedriver.exe");
+		Map<String, Object> prefs = new HashMap<String, Object>();
+		prefs.put("profile.default_content_setting_values.notifications", 2);
+		ChromeOptions options = new ChromeOptions();
+		options.setExperimentalOption("prefs", prefs);
+		driver=new ChromeDriver(options);
+		wait= new WebDriverWait(driver, 100);
+		driver.get("https://www.facebook.com/");
+	}
+	
+	//method for testing facebook login feature
+	 @Test
+	 public void facebookLoginTest() throws IOException, InterruptedException 
+	 {
+		  String filePath="D:\\";
+		  String fileName="login_credentials.xlsx";
+		  String sheetName="Sheet1";
+		  String[][] data=readExcelData(filePath,fileName,sheetName);
+		  String[] result=new String[5];
+		  
+		  for(int i=0;i<data.length;i++)
+		  {
+			  String id="";
+			  String paswd="";
+			  for(int j=0;j<data[1].length;j++)
+			  {
+				  if(j!=data[1].length-1)
+				  {
+					  id=data[i][j];
+				  }
+				  else if(j==data[1].length-1)
+				  {
+					  paswd=data[i][j];
+				  }
+			  }
+			  wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"email\"]")));
+			  WebElement userId=driver.findElement(By.xpath("//*[@id=\"email\"]"));
+			  userId.sendKeys(id);
+			  WebElement pswd=driver.findElement(By.xpath("//*[@id=\"pass\"]"));
+			  pswd.sendKeys(paswd);
+			  WebElement submit=driver.findElement(By.xpath("//*[@value=\"Log In\"]"));
+			  submit.click();
+			  Thread.sleep(100);
+			  String actual_title=driver.getTitle().substring(driver.getTitle().indexOf("F"));
+			  String expected_title="Facebook";
+			  if(actual_title.equalsIgnoreCase(expected_title))
+			  {
+				  System.out.println(id+"  "+paswd+"  "+"PASSED");
+				  result[i]="PASS";
+				  WebElement logout_nav=driver.findElement(By.xpath("//*[@id=\"userNavigationLabel\"]"));
+				  logout_nav.click();
+				  Thread.sleep(3000);
+				  WebElement logout_button=driver.findElement(By.xpath("//span[@class='_54nh'][contains(.,'Log Out')]"));
+				  logout_button.click();
+			  }
+			  else
+			  {
+				  System.out.println(id+"  "+paswd+"   "+"FAILED");
+				  result[i]="FAIL";
+				  driver.get("https://www.facebook.com/");
+			  }
+		  }
+		  
+		  writeExcelData(filePath,fileName,sheetName,result);
+		  
+	 }
+	 
+	 //method for terminating test and quitting driver
+	 @AfterTest
+	 public void tearDown()
+	 {
+		 driver.quit();
+	 }
+	 
+	 //method for reading user's credentials from excel sheet
+	 public String[][] readExcelData(String filePath,String fileName,String sheetName)throws  IOException
+	 {
+		 
+		 File file =    new File(filePath+fileName);
+		 FileInputStream inputStream = new FileInputStream(file);
+		 Workbook workbook = new XSSFWorkbook(inputStream);
+		 Sheet sheet = workbook.getSheet(sheetName);
+		 int rowCount = sheet.getLastRowNum()-sheet.getFirstRowNum();
+		 String[][] data=new String[rowCount][2];
+		 for(int i=0;i<data.length;i++)
+		 {
+			 Row row = sheet.getRow(i+1);
+			 System.out.println(row);
+			 for(int j=0;j<2;j++)
+			 {
+				 if(row.getCell(j).getCellType()==CellType.NUMERIC)
+				 {
+					
+					 data[i][j]=NumberToTextConverter.toText(row.getCell(j).getNumericCellValue());
+
+				 }
+				 else if(row.getCell(j).getCellType()==CellType.STRING)
+				 {
+				 data[i][j]=String.valueOf(row.getCell(j).getStringCellValue());
+				 }
+				 
+			 }
+				 
+		 }
+		 return data;
+	 }
+	 
+	 //method for writing result of test cases into excel sheet
+	 public void writeExcelData(String filePath,String fileName,String sheetName,String[] dataToWrite) throws IOException
+	 {
+	    File file =    new File(filePath+"\\"+fileName);
+        FileInputStream inputStream = new FileInputStream(file);
+		Workbook workbook = new XSSFWorkbook(inputStream);
+		Sheet sheet = workbook.getSheet(sheetName);
+		int rowCount = sheet.getLastRowNum()-sheet.getFirstRowNum();
+		for(int i=0;i<dataToWrite.length;i++)
+		 {
+			 Row row = sheet.getRow(i+1);
+		     Cell cell = row.createCell(2);
+		     cell.setCellValue(dataToWrite[i]);
+		 }
+		inputStream.close();
+		FileOutputStream outputStream = new FileOutputStream(file);
+		workbook.write(outputStream);
+	    outputStream.close();
+	 }
+}
